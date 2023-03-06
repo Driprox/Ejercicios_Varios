2c5a6f4eec4ecc061f1c004f6d4fc15d9cecfcf1
P <117913860+Driprox@users.noreply.github.com>
2023-03-06 03:19:08 +0100

    Update Nuevoarray.java

--- EjerciosMasJava/und2/Nuevoarray.java
+++ EjerciosMasJava/und2/Nuevoarray.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 public class Nuevoarray extends Thread {
 
   public void run(int lista[]) {
-
+//Listo y ordeno el array solicitado con las siguientes condiciones de accesso
     for (int e = 0; e < lista.length; e++) {
       if ((lista[e] >= 1) && (lista[e] <= 100)) {
         if (lista[e] % 2 == 0) {
