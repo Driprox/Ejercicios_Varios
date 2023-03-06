1ee65bd08a77704e1af7fada0848989c83bb5d14
P <117913860+Driprox@users.noreply.github.com>
2023-03-06 03:18:29 +0100

    Update Main.java

--- EjerciosMasJava/und2/Main.java
+++ EjerciosMasJava/und2/Main.java
@@ -11,17 +11,17 @@ import java.util.Scanner;
  * @author FP
  */
 public class Main {
-
+//Prorciona un aarray aleatorio de tamaño
   public static void main(String[] args) {
     Nuevoarray lista = new Nuevoarray();
     Nuevoarray lista2 = new Nuevoarray();
     Scanner sc = new Scanner(System.in);
     int[] numeros = new int[(int) (Math.random() * 100)];
-    for (int i = 0; i < numeros.length; i++) {
+    for (int i = 0; i < numeros.length; i++) {//introduce los numeros del array
       System.out.println("Introduce un numero: ");
       numeros[i] = sc.nextInt();
     }
-    lista.run(numeros);
+    lista.run(numeros);//uso el thread
     System.out.println("Siguiente array");
     lista2.run(numeros);
     
